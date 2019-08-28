5f2ee2f7ecd1336a7e2ff3dce3ff363bae1cedfa
Jorge Barcos <jorgebarcos88@gmail.com>
2019-08-28 13:38:51 -0300

    Mostrando la consulta en el HTML

--- js/app.js
+++ js/app.js
@@ -20,7 +20,20 @@ UI.formularioBuscar.addEventListener('submit', (e) => {
 		// El formulario esta completo, realizar consulta a la API
 		const api = new API(artista, cancion);
 		api.consultarAPI().then((data) => {
-			console.log(data);
+			if (data.respuesta.lyrics) {
+				// La canción existe
+				const letra = data.respuesta.lyrics;
+				UI.divResultado.textContent = letra;
+			} else {
+				// La canción no existe
+				UI.divMensajes.innerHTML = 'La canción No existe, prueba con otra búsqueda';
+				UI.divMensajes.classList.add('error');
+				setTimeout(() => {
+					UI.divMensajes.innerHTML = '';
+					UI.divMensajes.classList.remove('error');
+					UI.formularioBuscar.reset();
+				}, 3000);
+			}
 		});
 	}
 });
